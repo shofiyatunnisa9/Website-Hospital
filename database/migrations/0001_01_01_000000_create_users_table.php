9a5163eb8beadcd2501d07d96a1c1b5ce11d2e9f
shofiyatunnisa9 <shofiyatunnisa939@gmail.com>
2025-09-19 14:40:41 +0700

    add : table database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('photo');
+            $table->string('phone')->index();
+            $table->string('gender');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
