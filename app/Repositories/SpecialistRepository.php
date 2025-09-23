664421a4851259f979af7e69ada778bb323b31d1
shofiyatunnisa9 <shofiyatunnisa939@gmail.com>
2025-09-23 10:47:03 +0700

    feat : setup repositories, services and crud specialist

---
+++ app/Repositories/SpecialistRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Specialist;
+
+class SpecialistRepository
+{
+    public function gelAll(array $fields)
+    {
+        return Specialist::select($fields)->latest()->with(['hospitals', 'doctors'])->paginate(10);
+    }
+
+    public function getById(int $id, array $fields)
+    {
+        return Specialist::select($fields)
+            ->with([
+                'hospitals' => function ($q) use ($id) {
+                    $q->withCount(['doctors as doctors_count' => function ($query) use ($id) {
+                        $query->where('specialist_id', $id);
+                    }]);
+                },
+                'doctors' => function ($q) use ($id) {
+                    $q->where('specialist_id', $id)
+                        ->with('hospital:id,name,city,post_code');
+                }
+            ])
+            ->findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return Specialist::create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $specialist = Specialist::findOrFail($id);
+        $specialist->update($data);
+        return $specialist;
+    }
+
+    public function delete(int $id)
+    {
+        $specialist = Specialist::findOrFail($id);
+        $specialist->delete();
+    }
+}
